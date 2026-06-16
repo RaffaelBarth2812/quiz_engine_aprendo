0b2b85a4fd3e6fe7d51c2ba8ffc449730c9b1950
RaffaelBarth2812 <raffael.barth2812@gmail.com>
2026-06-16 08:47:12 +0200

    API: Routes fuer Anlegen und Abrufen von Quiz

--- quiz-engine/routes/api.php
+++ quiz-engine/routes/api.php
@@ -3,5 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\QuizController;
+
 Route::post('/quizzes', [QuizController::class, 'store']);
 Route::get('/quizzes/{quiz}', [QuizController::class, 'show']);
